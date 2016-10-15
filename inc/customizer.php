67fc92d0c90a191aac4f5d38c0cc5b696693c8da
voylinux <voylinux@gmail.com>
2016-10-15 03:01:29 +0200

    issue-1: First basic _s based structure

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * mh-theme Theme Customizer.
+ *
+ * @package mh-theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function mh_theme_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'mh_theme_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function mh_theme_customize_preview_js() {
+	wp_enqueue_script( 'mh_theme_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'mh_theme_customize_preview_js' );
